69296820cb3b13e526e701deeee79e0d3c9c692a
Hendrik Müller <henne90gen@gmail.com>
2023-03-14 22:22:56 +0100

    Solved l_39

--- main.go
+++ main.go
@@ -32,21 +32,56 @@ const (
 type InstructionType int
 
 const (
-	InvalidInstruction InstructionType = iota
-	MovRegMemToFromReg
-	MovImToRegMem
-	MovImToReg
-	MovMemToAcc
-	MovAccToMem
-	MovRegMemToSegReg
-	MovSegRegToRegMem
+	IT_Invalid InstructionType = iota
+	IT_MovRegMemToFromReg
+	IT_MovImToRegMem
+	IT_MovImToReg
+	IT_MovMemToAcc
+	IT_MovAccToMem
+	IT_MovRegMemToSegReg
+	IT_MovSegRegToRegMem
 )
+
+type AddressCalculationType int
+
+const (
+	ACT_Invalid AddressCalculationType = iota
+	ACT_BX_SI
+	ACT_BX_DI
+	ACT_BP_SI
+	ACT_BP_DI
+	ACT_SI
+	ACT_DI
+	ACT_BX
+	ACT_BX_SI_D8
+	ACT_BX_DI_D8
+	ACT_BP_SI_D8
+	ACT_BP_DI_D8
+	ACT_SI_D8
+	ACT_DI_D8
+	ACT_BP_D8
+	ACT_BX_D8
+	ACT_BX_SI_D16
+	ACT_BX_DI_D16
+	ACT_BP_SI_D16
+	ACT_BP_DI_D16
+	ACT_SI_D16
+	ACT_DI_D16
+	ACT_BP_D16
+	ACT_BX_D16
+)
+
+var addressCalculationTable = [][]AddressCalculationType{
+	{ACT_BX_SI, ACT_BX_DI, ACT_BP_SI, ACT_BP_DI, ACT_SI, ACT_DI, ACT_Invalid, ACT_BX},
+	{ACT_BX_SI_D8, ACT_BX_DI_D8, ACT_BP_SI_D8, ACT_BP_DI_D8, ACT_SI_D8, ACT_DI_D8, ACT_BP_D8, ACT_BX_D8},
+	{ACT_BX_SI_D16, ACT_BX_DI_D16, ACT_BP_SI_D16, ACT_BP_DI_D16, ACT_SI_D16, ACT_DI_D16, ACT_BP_D16, ACT_BX_D16},
+}
 
 type MemoryLocationType string
 
-var registerTable = []RegisterName{
-	AL, AX, CL, CX, DL, DX, BL, BX,
-	AH, SP, CH, BP, DH, SI, BH, DI,
+var registerTable = [][]RegisterName{
+	{AL, CL, DL, BL, AH, CH, DH, BH},
+	{AX, CX, DX, BX, SP, BP, SI, DI},
 }
 
 type Instruction struct {
@@ -60,38 +95,90 @@ type MemoryLocation struct {
 }
 
 func (t InstructionType) Name() string {
-	if t > InvalidInstruction && t < MovSegRegToRegMem {
+	if t > IT_Invalid && t < IT_MovSegRegToRegMem {
 		return "mov"
 	}
 
 	return ""
 }
 
+func (a AddressCalculationType) String(displacement int16) string {
+	switch a {
+	case ACT_BX_SI:
+		return "[bx + si]"
+	case ACT_BX_DI:
+		return "[bx + di]"
+	case ACT_BP_SI:
+		return "[bp + si]"
+	case ACT_BP_DI:
+		return "[bp + di]"
+	case ACT_SI:
+		return "[si]"
+	case ACT_DI:
+		return "[di]"
+	case ACT_BX:
+		return "[bx]"
+	case ACT_BX_SI_D8:
+		return fmt.Sprintf("[bx + si + %d]", displacement)
+	case ACT_BX_DI_D8:
+		return fmt.Sprintf("[bx + di + %d]", displacement)
+	case ACT_BP_SI_D8:
+		return fmt.Sprintf("[bp + si + %d]", displacement)
+	case ACT_BP_DI_D8:
+		return fmt.Sprintf("[bp + di + %d]", displacement)
+	case ACT_SI_D8:
+		return fmt.Sprintf("[si + %d]", displacement)
+	case ACT_DI_D8:
+		return fmt.Sprintf("[di + %d]", displacement)
+	case ACT_BP_D8:
+		return fmt.Sprintf("[bp + %d]", displacement)
+	case ACT_BX_D8:
+		return fmt.Sprintf("[bx + %d]", displacement)
+	case ACT_BX_SI_D16:
+		return fmt.Sprintf("[bx + si + %d]", displacement)
+	case ACT_BX_DI_D16:
+		return fmt.Sprintf("[bx + di + %d]", displacement)
+	case ACT_BP_SI_D16:
+		return fmt.Sprintf("[bp + si + %d]", displacement)
+	case ACT_BP_DI_D16:
+		return fmt.Sprintf("[bp + di + %d]", displacement)
+	case ACT_SI_D16:
+		return fmt.Sprintf("[si + %d]", displacement)
+	case ACT_DI_D16:
+		return fmt.Sprintf("[di + %d]", displacement)
+	case ACT_BP_D16:
+		return fmt.Sprintf("[bp + %d]", displacement)
+	case ACT_BX_D16:
+		return fmt.Sprintf("[bx + %d]", displacement)
+	}
+	return ""
+}
+
 func getInstructionType(b byte) (InstructionType, error) {
 	if b>>2 == 0b100010 {
 		// Register/memory to/from register
-		return MovRegMemToFromReg, nil
+		return IT_MovRegMemToFromReg, nil
 	} else if b>>1 == 0b1100011 {
 		// Immediate to register/memory
-		return MovImToRegMem, nil
+		return IT_MovImToRegMem, nil
 	} else if b>>4 == 0b1011 {
 		// Immediate to register
-		return MovImToReg, nil
+		return IT_MovImToReg, nil
 	} else if b>>1 == 0b1010000 {
 		// Memory to accumulator
-		return MovMemToAcc, nil
+		return IT_MovMemToAcc, nil
 	} else if b>>1 == 0b1010001 {
 		// Accumulator to memory
-		return MovAccToMem, nil
+		return IT_MovAccToMem, nil
 	} else if b == 0b10001110 {
 		// Register/memory to segment register
-		return MovRegMemToSegReg, nil
+		return IT_MovRegMemToSegReg, nil
 	} else if b == 0b10001100 {
 		// Segment register to register/memory
-		return MovSegRegToRegMem, nil
+		return IT_MovSegRegToRegMem, nil
 	}
 
-	return InvalidInstruction, fmt.Errorf("opcode %08b not implemented yet", b)
+	return IT_Invalid, fmt.Errorf("opcode %08b not implemented yet", b)
 }
 
 func assembleAndCompare(inputFileName string, inputFileContent []byte, result []byte) error {
@@ -164,7 +251,7 @@ func disassemble(content []byte) (string, error) {
 			return "", err
 		}
 
-		if instructionType == MovImToReg {
+		if instructionType == IT_MovImToReg {
 			w := (b1 >> 3) & 0b00000001
 			reg := b1 & 0b00000111
 
@@ -179,8 +266,7 @@ func disassemble(content []byte) (string, error) {
 				currentByte++
 			}
 
-			destinationRegisterIndex := reg<<1 + w
-			destinationRegisterName := registerTable[destinationRegisterIndex]
+			destinationRegisterName := registerTable[w][reg]
 			result += fmt.Sprintf("%s %s, %d\n", instructionType.Name(), destinationRegisterName, data)
 			continue
 		}
@@ -196,34 +282,74 @@ func disassemble(content []byte) (string, error) {
 
 		if mod == 0b11 {
 			// Register Mode (no displacement)
-			destinationRegisterIndex := rm<<1 + w
-			destinationRegisterName := registerTable[destinationRegisterIndex]
-			sourceRegisterIndex := reg<<1 + w
-			sourceRegisterName := registerTable[sourceRegisterIndex]
+			destinationRegisterName := registerTable[w][rm]
+			sourceRegisterName := registerTable[w][reg]
 			result += fmt.Sprintf("%s %s, %s\n", instructionType.Name(), destinationRegisterName, sourceRegisterName)
-		} else if mod == 0b01 {
-			// Memory Mode, 8 bit displacement follows
-			currentByte++
-		} else if mod == 0b10 {
-			// Memory Mode, 16 bit displacement follows
-			currentByte += 2
+			continue
 		} else {
-			// Memory Mode, no displacement follows (most of the time)
-			if rm == 0b110 {
+			// Memory Mode
+			if rm == 0b110 && mod == 0b11 {
+				print(result)
 				return "", errors.New("(rm == 0b110) not implemented yet")
+			}
+
+			addressCalculationType := addressCalculationTable[mod][rm]
+
+			displacement := int16(0)
+			if mod == 0b01 {
+				// Memory Mode, 8 bit displacement follows
+				displacement = int16(int8(content[currentByte]))
+				currentByte++
+			} else if mod == 0b10 {
+				// Memory Mode, 16 bit displacement follows
+				tmp := content[currentByte]
+				currentByte++
+
+				highByte := int16(content[currentByte])
+				displacement = int16(tmp) | (highByte << 8)
+				currentByte++
+			}
+
+			d := (b1 >> 1) & 0b00000001
+			if d == 0b1 {
+				destinationRegisterName := registerTable[w][reg]
+				result += fmt.Sprintf("%s %s, %s\n", instructionType.Name(), destinationRegisterName, addressCalculationType.String(displacement))
+				continue
+			} else {
+				sourceRegisterName := registerTable[w][reg]
+				result += fmt.Sprintf("%s %s, %s\n", instructionType.Name(), addressCalculationType.String(displacement), sourceRegisterName)
+				continue
 			}
 		}
 	}
 
-	println(result)
+	print(result)
 
 	return result, nil
 }
 
 func main() {
-	inputFiles := []string{"l_37", "l_38", "l_39"}
+	inputFiles := []string{"l_37.asm", "l_38.asm", "l_39.asm"}
 	for _, inputFile := range inputFiles {
-		content, err := os.ReadFile(inputFile)
+		cmd := exec.Command("nasm", inputFile)
+		stdout := new(strings.Builder)
+		stderr := new(strings.Builder)
+		cmd.Stdout = stdout
+		cmd.Stderr = stderr
+		err := cmd.Run()
+		if err != nil {
+			println(stdout.String())
+			println(stderr.String())
+			panic(err)
+		}
+
+		assembledInputFile := strings.TrimSuffix(inputFile, ".asm")
+		content, err := os.ReadFile(assembledInputFile)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Remove(assembledInputFile)
 		if err != nil {
 			panic(err)
 		}
